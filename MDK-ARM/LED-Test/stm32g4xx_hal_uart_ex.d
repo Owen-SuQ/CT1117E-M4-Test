aa3d32a9eed63197ae6ddcad6985994d5081096b
Owen-SuQ <98217048+Owen-SuQ@users.noreply.github.com>
2022-01-29 11:30:27 +0800

    不提交中间生成文件。提交结果文件

---
+++ MDK-ARM/LED-Test/stm32g4xx_hal_uart_ex.d
@@ -0,0 +1,36 @@
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Inc/stm32g4xx_hal_conf.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g431xx.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Include/core_cm4.h
+led-test\stm32g4xx_hal_uart_ex.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Include/cmsis_version.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+led-test\stm32g4xx_hal_uart_ex.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_ll_adc.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h
+led-test\stm32g4xx_hal_uart_ex.o: ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h
